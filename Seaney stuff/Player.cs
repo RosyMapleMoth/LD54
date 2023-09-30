142d68bde02f8ff21b9986036d1b36136323b4d3
Seaney F. Shell <sfs185@humboldt.edu>
2023-09-30 13:10:53 -0700

    minor update

---
+++ Seaney stuff/Player.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public class Player : KinematicBody
+{
+	[Export]
+	private float Speed;
+
+
+
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		SetMeta("Crushable", true);
+		SetMeta("Type", "Player");
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		var change = new Vector3(0,0,0);
+			
+		if (Input.IsActionPressed("player_move_right"))
+		{
+			change += new Vector3(Speed * delta, 0, 0);
+		}
+		if (Input.IsActionPressed("player_move_left"))
+		{
+			change += new Vector3(-Speed * delta, 0, 0);
+		}
+		if (Input.IsActionPressed("player_move_up"))
+		{
+			change += new Vector3(0, 0, -Speed * delta);
+		}
+		if (Input.IsActionPressed("player_move_down"))
+		{
+			change += new Vector3(0, 0, Speed * delta);
+		}
+		//Debug.WriteLine("Move Vector : " + change.ToString());
+		MoveAndCollide(change);
+	}
+
+	public void _on_Area_area_entered(object area)
+	{
+		Console.WriteLine("PowerUp collected");
+	}
+
+}
+
